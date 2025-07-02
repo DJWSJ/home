8c9a9c38f83c7ee70e80550d663b245fd00e0bbb
djwsj <2857439956@qq.com>
2025-07-03 04:16:42 +0800

    Merge remote-tracking branch 'origin/dev' into dev

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -4,8 +4,6 @@
     <!-- Logo -->
     <div class="logo">
       <div :class="{ name: true, 'text-hidden': true, long: siteUrl[0].length >= 6 }">
-        <span class="bg">{{ siteUrl[0] }}</span>
-        <span class="sm">.{{ siteUrl[1] }}</span>
       </div>
     </div>
     <!-- 简介 -->
@@ -40,7 +38,7 @@ const siteLogo = import.meta.env.VITE_SITE_MAIN_LOGO;
 // 站点链接
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "imsyy.top".split(".");
+  if (!url) return "djwsj.top".split(".");
   // 判断协议前缀
   if (url.startsWith("http://") || url.startsWith("https://")) {
     const urlFormat = url.replace(/^(https?:\/\/)/, "");
